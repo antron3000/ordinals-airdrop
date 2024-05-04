953f2bcd18b4a98c260d22c79878d7c5229b0721
stefan <stefan.braunovic55@gmail.com>
2024-05-04 13:57:00 +0200

    removed and optimized scripts

--- prepared-for-magiceden/scrapeErrorIds.js
+++ prepared-for-magiceden/scrapeErrorIds.js
@@ -47,7 +47,6 @@ function extractValues(content) {
   return values;
 }
 
-// Function to scrape with retry
 async function scrapeWithRetry(browser, url, retries = 3) {
   let attempt = 0;
   while (attempt < retries) {
@@ -73,68 +72,61 @@ async function scrapeWithRetry(browser, url, retries = 3) {
   }
 }
 
-async function scrapeAndExportData() {
-  // Check if scraped_data.json exists
-  if (fs.existsSync("scraped_data.json")) {
-    // Read the JSON file containing list of URLs
-    const urls = JSON.parse(fs.readFileSync("scraped_data.json"));
+// Read the JSON file containing list of URLs
+const urls = JSON.parse(fs.readFileSync("error_ids.json"));
 
-    const browser = await puppeteer.launch();
-    const batchSize = 500;
-    const totalUrls = urls.length;
-    const errorIds = [];
-    let allExtractedValues = [];
+(async () => {
+  const browser = await puppeteer.launch();
+  const batchSize = 500;
+  const totalUrls = urls.length;
+  const errorIds = [];
+  let allExtractedValues = [];
 
-    for (let i = 0; i < totalUrls; i += batchSize) {
-      const batchUrls = urls.slice(i, i + batchSize);
-      console.log(
-        `Processing URLs ${i + 1} to ${i + batchUrls.length} out of ${totalUrls}`
-      );
+  for (let i = 0; i < totalUrls; i += batchSize) {
+    const batchUrls = urls.slice(i, i + batchSize);
+    console.log(
+      `Processing URLs ${i + 1} to ${i + batchUrls.length} out of ${totalUrls}`
+    );
 
-      const scrapingTasks = batchUrls.map(async (url, index) => {
-        try {
-          const content = await scrapeWithRetry(
-            browser,
-            `https://ordinals.com/inscription/${url}`
-          );
-          if (content) {
-            // Extract specific values
-            const extractedValues = extractValues(content);
-            return extractedValues;
-          } else {
-            console.log(`Failed to scrape content for ${url}.`);
-            errorIds.push(url);
-            return null;
-          }
-        } catch (error) {
-          console.error(`Error scraping content for ${url}:`, error);
+    const scrapingTasks = batchUrls.map(async (url, index) => {
+      try {
+        const content = await scrapeWithRetry(
+          browser,
+          `https://ordinals.com/inscription/${url}`
+        );
+        if (content) {
+          // Extract specific values
+          const extractedValues = extractValues(content);
+          return extractedValues;
+        } else {
+          console.log(`Failed to scrape content for ${url}.`);
           errorIds.push(url);
           return null;
         }
-      });
+      } catch (error) {
+        console.error(`Error scraping content for ${url}:`, error);
+        errorIds.push(url);
+        return null;
+      }
+    });
 
-      const batchExtractedValues = await Promise.all(scrapingTasks);
-      allExtractedValues = allExtractedValues.concat(
-        batchExtractedValues.filter((value) => value !== null)
-      );
-    }
-
-    // Write all extracted values to a single JSON file
-    fs.writeFileSync(
-      "all_extracted_data2.json",
-      JSON.stringify(allExtractedValues, null, 2)
+    const batchExtractedValues = await Promise.all(scrapingTasks);
+    allExtractedValues = allExtractedValues.concat(
+      batchExtractedValues.filter((value) => value !== null)
     );
-
-    // Write error IDs to a separate JSON file
-    fs.writeFileSync("error_ids.json", JSON.stringify(errorIds, null, 2));
-
-    browser.close();
-    console.log(
-      `All URLs processed and data has been saved. Error IDs are saved in error_ids.json`
-    );
-  } else {
-    console.error("scraped_data.json does not exist. Please make sure the file exists before running the script.");
   }
-}
 
-module.exports = scrapeAndExportData;
+  // Write all extracted values to a single JSON file
+  fs.writeFileSync(
+    "all_extracted_data3.json",
+    JSON.stringify(allExtractedValues, null, 2)
+  );
+
+  // Write error IDs to a separate JSON file
+  fs.writeFileSync("error_ids1.json", JSON.stringify(errorIds, null, 2));
+
+  browser.close();
+  console.log(
+    `All URLs processed and data has been saved. Error IDs are saved in error_ids.json`
+  );
+})();
